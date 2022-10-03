26657bcdaafe8253ca09e1b6c760d06279118f3b
aurelien-oc-mentorship <65898532+aurelien-oc-mentorship@users.noreply.github.com>
2022-10-03 10:22:23 -0500

    Fix content for consistancy

--- pieces.js
+++ pieces.js
@@ -1,17 +1,20 @@
 import pieces from "./pieces-autos.js";
 
-const ampoule = pieces[0];
+const article = pieces[0];
+
+const pieceElement = document.createElement('article');
 const imageElement = document.createElement("img");
-imageElement.src = ampoule.image;
+imageElement.src = article.image;
 const nomElement = document.createElement("h2");
-nomElement.innerText = ampoule.nom;
+nomElement.innerText = article.nom;
 const prixElement = document.createElement("p");
-prixElement.innerText = `${ampoule.prix} € (${ampoule.prix < 35 ? '€' : '€€€'})`;
+prixElement.innerText = `${article.prix} € (${article.prix < 35 ? '€' : '€€€'})`;
 const categorieElement = document.createElement("p");
-categorieElement.innerText = ampoule.categorie ?? "(aucune catégorie)";
+categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
 
 const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
+sectionFiches.appendChild(pieceElement)
+pieceElement.appendChild(imageElement);
+pieceElement.appendChild(nomElement);
+pieceElement.appendChild(prixElement);
+pieceElement.appendChild(categorieElement);
