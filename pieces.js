173e7f1431c4ca50938c2a9e71ad1674030a1e87
aurelien-oc-mentorship <65898532+aurelien-oc-mentorship@users.noreply.github.com>
2022-10-03 10:13:01 -0500

    Adding for loop to create all elements

--- pieces.js
+++ pieces.js
@@ -1,23 +1,27 @@
 import pieces from "./pieces-autos.js";
 
-const ampoule = pieces[0];
-const imageElement = document.createElement("img");
-imageElement.src = ampoule.image;
-const nomElement = document.createElement("h2");
-nomElement.innerText = ampoule.nom;
-const prixElement = document.createElement("p");
-prixElement.innerText = `${ampoule.prix} € (${ampoule.prix < 35 ? '€' : '€€€'})`;
-const categorieElement = document.createElement("p");
-categorieElement.innerText = ampoule.categorie ?? "(aucune catégorie)";
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = ampoule.description ?? "Pas de description pour le moment.";
-const disponibleElement = document.createElement("p");
-disponibleElement.innerText = ampoule.disponibilite ? "En stock" : "Rupture de stock";
-
-const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(disponibleElement);
+for(let index = 0; index < pieces.length ; index++){
+    const article = pieces[index];
+    const imageElement = document.createElement("img");
+    imageElement.src = article.image;
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = article.nom;
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `${article.prix} € (${article.prix < 35 ? '€' : '€€€'})`;
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+    const disponibleElement = document.createElement("p");
+    disponibleElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+    
+    const sectionFiches = document.querySelector(".fiches");
+    const pieceElement = document.createElement('article');
+    sectionFiches.appendChild(pieceElement)
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(disponibleElement);
+}
