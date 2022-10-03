f920577a17671d43fb50cdce21e4a6ab351234d3
aurelien-oc-mentorship <65898532+aurelien-oc-mentorship@users.noreply.github.com>
2022-10-03 09:23:01 -0500

    Add course content for chapter 4

---
+++ pieces.js
@@ -0,0 +1,17 @@
+import pieces from "./pieces-autos.js";
+
+const ampoule = pieces[0];
+const imageElement = document.createElement("img");
+imageElement.src = ampoule.image;
+const nomElement = document.createElement("h2");
+nomElement.innerText = ampoule.nom;
+const prixElement = document.createElement("p");
+prixElement.innerText = `${ampoule.prix} € (${ampoule.prix < 35 ? '€' : '€€€'})`;
+const categorieElement = document.createElement("p");
+categorieElement.innerText = ampoule.categorie ?? "(aucune catégorie)";
+
+const sectionFiches = document.querySelector(".fiches");
+sectionFiches.appendChild(imageElement);
+sectionFiches.appendChild(nomElement);
+sectionFiches.appendChild(prixElement);
+sectionFiches.appendChild(categorieElement);
