91e9f1ada576ade10c5ecb644faefb22287ff576
aurelien-oc-mentorship <65898532+aurelien-oc-mentorship@users.noreply.github.com>
2022-10-04 06:20:04 -0500

    Keep option 1

--- pieces.js
+++ pieces.js
@@ -63,34 +63,6 @@ boutonFiltrer.addEventListener("click", function () {
         .appendChild(abordablesElements)
 });
 
-/*
-// OPTION 2
-boutonFiltrer.addEventListener("click", function () {
-    const noms = pieces.map(piece => piece.nom); 
-    //boucle for de la fin vers le début
-    for(let i = pieces.length -1 ; i >= 0; i--){
-        if(pieces[i].prix > 35){
-            noms.splice(i,1)
-        }
-    } 
-    //Création de l'en-tête 
-    const pElement = document.createElement('p')
-    pElement.innerText = "Pièces abordables";
-    //Création de la liste
-    const abordablesElements = document.createElement('ul');
-    //Ajout de chaque nom à la liste
-    for(let i=0; i < noms.length ; i++){
-        const nomElement = document.createElement('li');
-        nomElement.innerText = noms[i];
-        abordablesElements.appendChild(nomElement)
-    }
-    // Ajout de l'en-tête puis de la liste au bloc résultats filtres
-    document.querySelector('.resultats-filtres')
-        .appendChild(pElement)
-        .appendChild(abordablesElements)
-});
-*/
-
 const boutonFiltrerDescription = document.querySelector(".btn-filtrer-description");
 
 boutonFiltrerDescription.addEventListener("click", function () {
